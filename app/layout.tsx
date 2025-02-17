dc2650a63bf0bcfcd2ea69b28e6ab7905ab005ab
KsmDabin <dabinko@kita.net>
2025-02-17 20:22:05 +0900

    Update footer to remove mobile contact message

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from '../components/Footer'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+} 
